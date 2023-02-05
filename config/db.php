e58c4cd51b9cc6b5ea358cc748be4d4727af980d
nffano <124452828+nffano@users.noreply.github.com>
2023-02-05 14:47:40 +0700

    5-Gii Module CRUD Application
    5-Gii Module CRUD Application

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=basicdb',
+    'dsn' => 'mysql:host=localhost;dbname=yii_tutorial',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
